bd04532d0a8042d60579518045d5a6ae64899609
ck520702002 <ck520702002@gmail.com>
2017-01-30 18:51:24 +0800

    rewrite the algorithm

--- apt.py
+++ apt.py
@@ -2,10 +2,10 @@ import pdb
 import itertools
 import copy
 import operator
-
+from multiprocessing import Pool
 #file opening
-filename = raw_input("Enter the inputfile name: ")
-f = open(filename)
+#filename = raw_input("Enter the inputfile name: ")
+f = open('inputfile3.txt')
 
 #data cleaning
 a = [x for x in f.readlines() if x != '\r\n']
@@ -89,7 +89,7 @@ personalities = [{'type':'Money Grubber','pay':10,'hours':-1,'impact':4,'learn':
 				 {'type':'Academic','pay':2,'hours':-6,'impact':8,'learn':10}]
 
 
-#this is a function which calculates the bonus between 2 given people
+#this is a function that calculates the bonus between 2 given people
 def relationshipBonus (name1, name2):
 	for relationship in relationships:
 		if (name1 == relationship['p1'] and name2 == relationship['p2']) or (name2 == relationship['p1'] and name1 == relationship['p2']):
@@ -102,6 +102,19 @@ def relationshipBonus (name1, name2):
 			if relationship['relationship'] == 'Married':  
 				return 1000
 	return 0
+#this is a function that returns the possible locations for 2 married people since they must be together
+def possibleLocations (name1, name2):
+	temp1 = []
+	temp2 = []
+	for offer in offers:
+		if offer['name'] == name1:
+			if offer['location'] not in []:
+				temp1.append(offer['location'])
+		elif offer['name'] == name2:
+			if offer['location'] not in []:
+				temp2.append(offer['location'])
+	return list(set(temp2).intersection(temp1))
+ 
 
 #caculate the utility for each offer in the offers list
 for person in people:
@@ -113,20 +126,117 @@ for person in people:
 						if benefit['type'] == offer['type']:
 							offer['utility'] = benefit['pay']*personality['pay']+benefit['hours']*personality['hours']+benefit['impact']*personality['impact']+benefit['learn']*personality['learn']
 
-#create a list of all possible combinations of assignments 
-#if Amy has 3 choices, Bob has 2, and Charlie has 2, then the number of all possible combinations is 3*2*2 = 12
+#put the offers into personalOffer -> Amy's offers[{..},{..}]
+#make a list of personalOffers -> [ [{..},{..}], [{..},{..}] .. ] 
 personalOffers = []
 for person in people:
-	temp = []
-	for offer in offers:
-		if offer['name'] == person['name']:
-			temp.append(offer)
-	personalOffers.append(temp)
-combinations = map(list,itertools.product( *personalOffers))
+	personalOffers.append([offer for offer in offers if offer['name'] == person['name']])
+
+
+#--------
+#eliminates the useless offers from each personalOffer since the married ones must be in the same city
+#newPersonalOffers=[]
+#marriedNameList = []
+#for relationship in relationships:
+#	if relationship['relationship'] == 'Married':
+#		marriedNameList.append(relationship['p1'])
+#		marriedNameList.append(relationship['p2'])
+#		locations = possibleLocations(relationship['p1'],relationship['p2'])
+#		for personalOffer in personalOffers:
+#			if personalOffer[0]['name'] == relationship['p1'] or personalOffer[0]['name'] == relationship['p2']:
+#				newPersonalOffers.append([offer for offer in personalOffer if offer['location'] in locations ])
+#
+#create a list containing the names of the modified personalOffers
+#marriedNameList = [ newPersonalOffer[0]['name'] for newPersonalOffer in newPersonalOffers ]
+#
+#create a temp list to update the list of personalOffers
+#temp  = [ personalOffer for personalOffer in personalOffers if personalOffer[0]['name'] not in marriedNameList ]
+#for newPersonalOffer in newPersonalOffers:
+#	temp.append(newPersonalOffer)
+
+#update personalOffers
+#personalOffers = temp
+#------------
+
+#x=0
+#for i in personalOffers:
+#	x = x+len(i)
+#print 'reduced ' + str(len(offers)-x) +' offers by the contraint that the married people should be in the same city'
+
+enemyOffers = []
+marriedOffers =[]
+marriedNameList =[]
+mortalNameList = []
+for relationship in relationships:
+	if relationship['relationship'] == 'Married' and relationship['p1'] not in marriedNameList:
+			marriedNameList.append(relationship['p1'])
+			marriedNameList.append(relationship['p2'])
+for relationship in relationships:
+	if relationship['relationship'] == 'Mortal Enemies' and relationship['p1'] not in mortalNameList and relationship['p1'] not in marriedNameList and relationship['p2'] not in marriedNameList:
+			mortalNameList.append(relationship['p1'])
+			mortalNameList.append(relationship['p2'])
+
+for relationship in relationships:
+	if relationship['relationship'] == 'Married':
+		marriedOffer = [personalOffer for personalOffer in personalOffers if personalOffer[0]['name'] == relationship['p1']  or personalOffer[0]['name'] == relationship['p2']]
+		marriedOffers.append(marriedOffer)
+		
+	elif relationship['relationship'] == 'Mortal Enemies':	
+		if relationship['p1'] not in marriedNameList and relationship['p2'] not in marriedNameList:
+			enemyOffer = [personalOffer for personalOffer in personalOffers if personalOffer[0]['name'] == relationship['p1']  or personalOffer[0]['name'] == relationship['p2']]
+			enemyOffers.append(enemyOffer)
+
+possibleMarriedCombinationSet =[]
+for marriedOffer in marriedOffers:
+	marriedCombinations = map(list,itertools.product( *marriedOffer))
+	possibleCombinations =[combination for combination in marriedCombinations if combination[0]['location'] == combination[1]['location']]
+	possibleMarriedCombinationSet.append(possibleCombinations)
+
+possibleEnemyCombinationSet=[]
+for enemyOffer in enemyOffers:
+	enemyCombinations = map(list,itertools.product( *enemyOffer))
+	possibleCombinations =[combination for combination in enemyCombinations if combination[0]['location'] != combination[1]['location']]
+	possibleEnemyCombinationSet.append(possibleCombinations)
+
+normalOffers = [personalOffer for personalOffer in personalOffers if personalOffer[0]['name'] not in marriedNameList and personalOffer[0]['name'] not in mortalNameList ]
+normalCombinations = map(list,itertools.product( *normalOffers))
+
+resultCombinations = normalCombinations
+for possibleMarriedCombination in possibleMarriedCombinationSet:
+	templist= [possibleMarriedCombination, resultCombinations]
+	resultCombinations = map(list,itertools.product( *templist))
+
+for possibleEnemyCombination in possibleEnemyCombinationSet:
+	templist= [possibleEnemyCombination, resultCombinations]
+	resultCombinations = map(list,itertools.product( *templist))
+
+finalCombinations = []
+for resultCombination in resultCombinations:
+	combination =[]
+	for offerSet in resultCombination:
+		for offer in offerSet:
+			combination.append(offer)
+	finalCombinations.append(combination)
+
+
+
+
+
+	
+
+#combinations=[]
+#combinations = map(list,itertools.product( *personalOffers))
+#pool = Pool(500)
+#count=0
+#for result in pool.imap(list, itertools.product(*personalOffers	)):
+#    combinations.append(result)
+#    print len(combinations)
+
+
 
 #for each combination, calculate the utility for each offer considering the geography
 results = []
-for combination in combinations:
+for combination in finalCombinations:
 	assignments = []
 	total = 0
 	for offer in combination:
@@ -148,3 +258,5 @@ optimal = results.pop()
 print ("the optimal assignments are:")
 for offer in optimal['assignments']:
 	print (offer['name'] + ' --> ' + offer['company'])
+
+pdb.set_trace()
